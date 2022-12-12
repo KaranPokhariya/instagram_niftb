03824db93185b068bd6aeed9e3ceb0f5ba2f2390
Karan Singh Pokhariya <karanpokhariya10@gmail.com>
2022-12-12 20:36:42 +0530

    Update follow.py

--- follow.py
+++ follow.py
@@ -41,9 +41,9 @@ password = WebDriverWait(driver, 10).until(
 
 #enter username and password
 username.clear()
-username.send_keys("presidency_blr_confessions")
+username.send_keys("confessions_niftb")
 password.clear()
-password.send_keys("qwerty.1234")
+password.send_keys("Coinmint@123")
 
 #target the login button and click it
 button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
